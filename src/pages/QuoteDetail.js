5eed8f4ac8d22ee69f3e83cb7f953c56c72624ab
rootsuke <rootsuke@gmail.com>
2021-08-26 01:40:26 +0900

    add dummy data and pages to render them

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,27 @@
 import { Route, useParams } from "react-router-dom"
 import Comments from "../components/comments/Comments"
+import HighlightedQuote from "../components/quotes/HighlightedQuote"
+
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Matsumu', text: 'cho!!!'},
+  { id: 'q2', author: 'Mattyun', text: 'ban!!'},
+  { id: 'q3', author: 'Ryotaro', text: 'warikan de !!!'},
+  { id: 'q4', author: 'Matsumu', text: 'urusaiwa!!!'},
+]
 
 const QuoteDetail = () => {
   const params = useParams()
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+  if (!quote) {
+    return <p>No quote found.</p>
+  }
   
   return (
     <>
-      <h1>QuoteDetail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote author={quote.author} text={quote.text}></HighlightedQuote>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments></Comments>
       </Route>
--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,6 +1,15 @@
+import QuoteList from '../components/quotes/QuoteList'
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Matsumu', text: 'cho!!!'},
+  { id: 'q2', author: 'Mattyun', text: 'ban!!'},
+  { id: 'q3', author: 'Ryotaro', text: 'warikan de !!!'},
+  { id: 'q4', author: 'Matsumu', text: 'urusaiwa!!!'},
+]
+
 const Quotes = () => {
   return (
-    <h1>Quotes</h1>
+    <QuoteList quotes={DUMMY_QUOTES}></QuoteList>
   )
 }
 
